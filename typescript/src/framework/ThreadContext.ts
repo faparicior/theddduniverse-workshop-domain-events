d7448dc448ede3613358305e6ba2ca1b015cb7bb
Fernando Aparicio <faparicior@gmail.com>
2025-03-01 18:11:48 +0100

    Fix Threadcontext in typescript

--- typescript/src/framework/ThreadContext.ts
+++ typescript/src/framework/ThreadContext.ts
@@ -5,17 +5,36 @@ export class ThreadContext {
 
     public static setValue(key: string, value: any): void {
         const store = ThreadContext.storage.getStore();
-        if (store) {
-            store.set(key, value);
+
+        if (!store) {
+            throw new Error('No store available. Did you forget to call ThreadContext.run()?');
         }
+        store.set(key, value);
     }
 
     public static getValue(key: string): any | undefined {
         const store = ThreadContext.storage.getStore();
+        if (!store) {
+            throw new Error('No store available. Did you forget to call ThreadContext.run()?');
+        }
+
         return store ? store.get(key) : undefined;
     }
 
     public static run(callback: () => void): void {
         ThreadContext.storage.run(new Map(), callback);
+    }
+
+    public static async runAsync<T>(callback: () => Promise<T>): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            ThreadContext.storage.run(new Map(), async () => {
+                try {
+                    const result = await callback();
+                    resolve(result);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        });
     }
 }
